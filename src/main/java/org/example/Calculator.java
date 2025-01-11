0da9181e070ada9836c4e22ae28abb9740069f85
AbdallahOb <abdallah.bus2001@gmail.com>
2025-01-11 15:47:00 +0300

    fixing divide method

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -13,8 +13,15 @@ public class Calculator {
         return num1*num2;
     }
 
-    public static int divideTwoNumbers(int num1, int num2){
-        return num1/num2;
+    public static double divideTwoNumbers(int num1, int num2){
+        double result = 0;
+        try{
+            result = num1/num2;
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        }
+
+        return result;
     }
 
     public static int moduloTwoIntegerNumbers(int num1, int num2){
