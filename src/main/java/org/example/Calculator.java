1b101b42b97247b973c6fe66bcfbbe67b08792fe
AbdallahOb <abdallah.bus2001@gmail.com>
2025-01-11 15:16:41 +0300

    add, subtract methods completed

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Calculator {
+    public static int addTwoNumbers(int num1, int num2){
+        return num1+num2;
+    }
+
+    public static int subtractTwoNumbers(int num1, int num2){
+        return num1-num2;
+    }
+}
